959da1729c6ab16b0b8f1c53552d842db852cf21
Shadowdare <shadowdare@users.noreply.github.com>
2014-12-28 15:53:45 -0800

    Clean up

--- Files/Controllers/FilesController.cs
+++ Files/Controllers/FilesController.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.Caching;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using Files.ViewModels;
-using PagedList;
-using File = Files.Models.File;
 
 namespace Files.Controllers
 {
@@ -18,7 +10,7 @@ namespace Files.Controllers
         [Route("files/{page:int?}")]
         public ActionResult Index(int? page)
         {
-            ViewBag.Files = Files.Models.File.GetFiles(Server.MapPath("~/uploads"), page ?? 1);
+            ViewBag.Files = Models.File.GetFiles(Server.MapPath("~/uploads"), page ?? 1);
 
             return View();
         }
@@ -27,16 +19,6 @@ namespace Files.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Upload(FileViewModels.UploadViewModel upload)
         {
-            //// Verify that the user selected a file
-            //if (file != null && file.ContentLength > 0)
-            //{
-            //    // extract only the fielname
-            //    var fileName = Path.GetFileName(file.FileName);
-            //    // store the file inside ~/App_Data/uploads folder
-            //    var path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
-            //    file.SaveAs(path);
-            //}
-
             if (ModelState.IsValid)
             {
                 //if ((upload.File != null) && (upload.File.ContentLength > 0))
@@ -47,9 +29,9 @@ namespace Files.Controllers
                 //}
             }
 
-            ViewBag.Files = Files.Models.File.GetFiles(Server.MapPath("~/uploads"));
+            ViewBag.Files = Models.File.GetFiles(Server.MapPath("~/uploads"));
 
-            return View("Index", upload);        
+            return View("Index", upload);
         }
     }
 }
